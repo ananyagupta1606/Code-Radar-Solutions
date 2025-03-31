a9d8f921322eefa79a2390ba64d163c243355dd9
Ananya Gupta <ananyag1606@gmail.com>
2025-04-01 00:19:45 +0530

    Solution to Fibonacci Series

--- Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -5,7 +5,7 @@ int fibonacciSeries(int n){
     printf("%d", fib[0]);
     printf("%d", fib[1]);
     for(int i = 2; i < n; i++){
-        fib[i] = fib[i-1] = fib[i-2];
+        fib[i] = fib[i-1] + fib[i-2];
         printf("%d ", fib[i]);
         
     }
